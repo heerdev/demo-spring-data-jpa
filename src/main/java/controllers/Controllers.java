480c38930cc2ce1504f994f2c539cc5035ac504a
Shagufta Naz <heer@192.168.1.103>
2016-12-22 00:12:55 +0530

    post request

--- src/main/java/controllers/Controllers.java
+++ src/main/java/controllers/Controllers.java
@@ -6,7 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.heer.repositories.ProductRepo;
@@ -22,25 +25,36 @@ public class Controllers implements ErrorController {
 	private ProductRepo products;
 
 	@RequestMapping("/index")
-	public String Home(){
+	public @ResponseBody String Home(){
 		return "Hello";
 	}
 	
 	@RequestMapping("/productlist")
-	public List<Product> ProductList() {
+	public @ResponseBody List<Product> ProductList() {
 		log.info("The product list :" + products.findAll().toString());
 		return products.findAll();
 		
 	}
 	
 	@RequestMapping(value = PATH)
-    public String error() {
+    public @ResponseBody String error() {
         return "Some Error has occured, please refer to the support!";
     }
 
     public String getErrorPath() {
         return PATH;
     }
+    
+	@RequestMapping(value="/addproduct", method=RequestMethod.POST, headers = {"Content-type=application/json"})
+	public @ResponseBody Product addProduct(
+			@RequestBody final Product product) {
+		log.info("The product list :" + products.findAll().toString());
+		
+		return products.save(product);
+		
+	}
+    
+    
 	}
 	
 	
